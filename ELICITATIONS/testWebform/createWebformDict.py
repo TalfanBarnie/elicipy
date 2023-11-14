be6ad9d2a2db1a4aee2ec78eafb6523b92b46a79
Talfan Barnie <TalfanBarnie@users.noreply.github.com>
2023-11-14 14:14:40 +0000

    Update createWebformDict.py

--- ELICITATIONS/testWebform/createWebformDict.py
+++ ELICITATIONS/testWebform/createWebformDict.py
@@ -10,10 +10,10 @@ group_list = ['Group A', 'Group B', 'Group C']
 absolute_indexing = True
 
 # select 'github' or 'local'
-datarepo = 'local'
+# datarepo = 'local'
 
-# datarepo = 'github'
-# RepositoryData = 'createWebformAnswers'
+datarepo = 'github'
+RepositoryData = 'createWebformAnswers'
 
 # encrypted = False
 
